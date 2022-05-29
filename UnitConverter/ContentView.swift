464a01191c15041813e4421ae563360528f20a86
Gabriel Marquez <gabriel@marquezgabriel.com>
2022-05-28 20:06:25 -0400

    Start UI layout

--- UnitConverter/ContentView.swift
+++ UnitConverter/ContentView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @FocusState private var amountIsFocused: Bool
+    @State private var numberToConvert: Double = 0.0
+    
+    let unitsType = ["Milimeters", "Meters", "Miles"]
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField ("Type the number your want to convert", value: $numberToConvert, format: .number)
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    
+                    
+                    
+                    
+                }
+                Section {
+                    Text (numberToConvert, format: .number)
+                }
+            }
+            .navigationTitle("Unit Converter")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+            .previewDevice("iPhone 11 Pro Max")
     }
 }
