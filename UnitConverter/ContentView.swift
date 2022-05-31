d9510a19f19163da0b0a046a19f574677004a329
Gabriel Marquez <gabriel@marquezgabriel.com>
2022-05-31 10:19:01 -0400

    Adopt API's build into iOS to handle conversion

--- UnitConverter/ContentView.swift
+++ UnitConverter/ContentView.swift
@@ -9,43 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var input = 1.0
-    @State private var inputUnit = "Meters"
-    @State private var outputUnit = "Kilometers"
+    @State private var inputUnit = UnitLength.meters
+    @State private var outputUnit = UnitLength.kilometers
     
-    let units = ["Feet", "Kilometers", "Meters", "Miles", "Yards"]
+    let units: [UnitLength] = [.feet, .kilometers, .meters, .miles, .yards]
+    let formatter: MeasurementFormatter
+    
+    init() {
+        formatter = MeasurementFormatter()
+        formatter.unitOptions = .providedUnit
+        formatter.unitStyle = .long
+    }
+    
     
     var result: String {
-        let inputToMetersMultiplier: Double
-        let metersToOutputMultiplier: Double
-        
-        switch inputUnit {
-        case "Feet":
-            inputToMetersMultiplier = 0.3048
-        case "Kilometers":
-            inputToMetersMultiplier = 1000
-        case "Miles":
-            inputToMetersMultiplier = 1609.34
-        case "Yards":
-            inputToMetersMultiplier = 0.9144
-        default:
-            inputToMetersMultiplier = 1.0
-        }
-        switch outputUnit {
-        case "Feet":
-            metersToOutputMultiplier = 3.28084
-        case "Kilometers":
-            metersToOutputMultiplier = 0.001
-        case "Miles":
-            metersToOutputMultiplier = 0.000621371
-        case "Yards":
-            metersToOutputMultiplier = 1.09361
-        default:
-            metersToOutputMultiplier = 1.0
-        }
-        let inputInMeters = input * inputToMetersMultiplier
-        let output = inputInMeters * metersToOutputMultiplier
-        let outputString = output.formatted()
-        return "\(outputString) \(outputUnit.lowercased())"
+        let inputMeasument = Measurement(value: input, unit: inputUnit)
+        let outputMeasurement = inputMeasument.converted(to: outputUnit)
+        return formatter.string(from: outputMeasurement)
     }
     
     @FocusState private var inputIsFocused: Bool
@@ -65,14 +45,14 @@ struct ContentView: View {
                 }
                 Picker("Convert from", selection: $inputUnit) {
                     ForEach(units, id: \.self) {
-                        Text($0)
+                        Text(formatter.string(from: $0).capitalized)
                     }
-                }.pickerStyle(.segmented)
+                }.pickerStyle(.automatic)
                 Picker("Convert to" ,selection: $outputUnit) {
                     ForEach(units, id: \.self) {
-                        Text($0)
+                        Text(formatter.string(from: $0).capitalized)
                     }
-                } .pickerStyle(.segmented)
+                } .pickerStyle(.automatic)
                 
                 Section {
                     Text(result)
